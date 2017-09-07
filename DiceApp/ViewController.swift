760780a71c5e1da115e8d862dd7cd09e11fafe38
Tolunay Akyurek <tolunayakyurek@sabanciuniv.edu>
2017-09-07 16:07:00 +0300

    DiceApp for iOS
    Simple dice application for iOS platform

--- DiceApp/ViewController.swift
+++ DiceApp/ViewController.swift
@@ -2,17 +2,29 @@
 //  ViewController.swift
 //  DiceApp
 //
-//  Created by tolunay akyürek on 02/09/2017.
+//  Created by tolunay akyurek on 02/09/2017.
 //  Copyright © 2017 akyurekt. All rights reserved.
 //
 
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    
+    var randomDiceIndex1 : Int = 0;
+    var randomDiceIndex2 : Int = 0;
+    
+    let diceArray = ["dice1","dice2","dice3","dice4","dice5","dice6"]
+    
+    @IBOutlet weak var diceImageView1: UIImageView!
+    @IBOutlet weak var diceImageView2: UIImageView!
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        updateDiceImages();
     }
 
     override func didReceiveMemoryWarning() {
@@ -21,5 +33,27 @@ class ViewController: UIViewController {
     }
 
 
+    @IBAction func rollButtonPressed(_ sender: UIButton) {
+        
+        updateDiceImages();
+        
+    }
+    
+    func updateDiceImages(){
+        
+        
+        randomDiceIndex1 = Int(arc4random_uniform(6));
+        randomDiceIndex2 = Int(arc4random_uniform(6));
+        
+        diceImageView1.image = UIImage(named: diceArray[randomDiceIndex1]);
+        diceImageView2.image = UIImage(named: diceArray[randomDiceIndex2]);
+        
+    
+    }
+    
+    override func motionEnded(_ motion: UIEventSubtype, with event: UIEvent?) {
+        
+        updateDiceImages();
+    }
 }
 
